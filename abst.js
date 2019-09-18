7051a7d12ed0ee51821694342d8451ed73f9a491
Darshan G M <darshan.gm9@gmail.com>
2019-09-18 16:22:15 +0530

    Update abst.js

--- abst.js
+++ abst.js
@@ -1,8 +1,8 @@
 function AbsFunction() {
   var x = document.getElementById("myDIV");
   if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
